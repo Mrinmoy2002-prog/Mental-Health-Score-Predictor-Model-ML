299d7a476b3ce8f17bdf5375fdb19b1b7ba66780
Mrinmoy2002-prog <mrinmoy.low2002@gmail.com>
2026-09-25 12:19:50 +0530

    backend is deployed in render and in script.js the backend link is updated

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Backend API Endpoint URL
-  const API_URL = 'http://127.0.0.1:8000/predict';
+//   const API_URL = 'http://127.0.0.1:8000/predict';
+  const API_URL = 'https://mental-health-score-predictor-model-ml.onrender.com'; // Render URL for deployed Backend 
 
   // Element Selectors
   const form = document.getElementById('prediction-form');
